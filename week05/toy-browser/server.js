f74bcbbeb2c5f4d5f6f78caeee49d11ffad47295
EdieFu <63287146+EdieFu@users.noreply.github.com>
2020-05-13 13:52:08 +0800

    Create server.js

---
+++ week05/toy-browser/server.js
@@ -0,0 +1,42 @@
+/*
+ http://nodejs.org/dist/latest-v12.x/docs/api/http.html#http_class_http_clientrequest
+ HTTP
+    class_http_clientrequest
+        Event: 'connect'代码部分客户端：
+         // Make a request to a tunneling proxy
+            const options = {
+             port: 1337,
+             host: '127.0.0.1',
+             method: 'CONNECT',
+             path: 'www.google.com:80'
+            };
+
+            const req = http.request(opt  …………
+
+    createServer服务器端
+         // Returns content-type = text/plain
+        const server = http.createServer((req, res) => {
+        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('X-Foo', 'bar');
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('ok');
+        });
+      
+*/
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    console.log("request received");
+    console.log(req.headers);
+    //console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('X-Foo', 'bar');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('ok');
+});
+
+server.listen(8088);
+
+
+
